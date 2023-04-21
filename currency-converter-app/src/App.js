a33a60fd988c7c379fc2fd74ece0828971824298
gautham-hub001 <gautham2pothana@gmail.com>
2023-04-21 18:57:35 -0400

    updating conversion based on currency selected by user

--- currency-converter-app/src/App.js
+++ currency-converter-app/src/App.js
@@ -8,8 +8,42 @@ const API_KEY = 'P6TDEvJk3fTnbl7mv5thKCzET38SxnHL';
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState([]);
-  const [toCurrency, setToCurrency] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState([]); // This is the value of first dropdown
+  const [toCurrency, setToCurrency] = useState([]); // This is the value of second dropdown
+  const [amount, setAmount] = useState(1) // This is going to store the amount updated by the user (first/second input)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true) // true if fromCurrency was updated, false if toCurrency was updated.
+  const [exchangeRate, setExchangeRate] = useState();
+  // console.log(data.rates)
+  console.log(exchangeRate)
+
+  let fromAmount, toAmount
+  if(amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  }
+  else {
+    fromAmount = amount / exchangeRate
+    toAmount = amount
+  }
+
+  // updating fromCurrency and toCurrency whenever selectedCurrency is updated by the user
+  useEffect(() => {
+    // Don't fetch if fromCurrency or toCurrency are undefined.
+    if(fromCurrency != null && toCurrency != null) {
+      fetch(`${CURRENCY_RATE_URL}?symbols=${toCurrency}&base=${fromCurrency}`, requestOptions)
+      .then(res => res.json())
+      .then(data => setExchangeRate(data.rates[toCurrency]))
+    }
+  }, [fromCurrency, toCurrency])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
 
   let myHeaders = new Headers();
   myHeaders.append("apikey", API_KEY);
@@ -29,14 +63,15 @@ function App() {
         setFromCurrency(data.base); // EUR
         const firstCurrencyFromOptions = Object.keys(data.rates)[0];
         setToCurrency(firstCurrencyFromOptions); // AED
+        setExchangeRate(data.rates[firstCurrencyFromOptions])
       })
   }, [])
   return (
     <>
     <h1>Currency Converter</h1>
-    <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={fromCurrency} onChangeCurrency={e => setFromCurrency(e.target.value)}></CurrencyRow>
+    <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={fromCurrency} onChangeCurrency={e => setFromCurrency(e.target.value)} amount={fromAmount} onChangeAmount={handleFromAmountChange}></CurrencyRow>
     <div class="equals">=</div>
-    <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency} onChangeCurrency={e => setToCurrency(e.target.value)}></CurrencyRow>
+    <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency} onChangeCurrency={e => setToCurrency(e.target.value)} amount={toAmount} onChangeAmount={handleToAmountChange}></CurrencyRow>
     </>
   );
 }
--- currency-converter-app/src/CurrencyRow.js
+++ currency-converter-app/src/CurrencyRow.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function CurrencyRow(props) {
-  const {currencyOptions, selectedCurrency, onChangeCurrency} = props
+  const {currencyOptions, selectedCurrency, onChangeCurrency, amount, onChangeAmount} = props
   return (
     <>
-    <input type="number" className='input'></input>
+    <input type="number" className='input' value={amount} onChange={onChangeAmount}></input>
     {/* Everytime the dropdown option is changed by the user, onChangeCurrency function is going to be called with an event.
         This function modifies selectedCurrency which updates the <select> tag's value.
     */}
